f8cef237a2034d196f7b4c04fa06e007bbae9888
Vano <lapitski.ivan@mail.ru>
2022-11-09 21:11:06 +0300

    'requirements.txt'

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-xpdma2^7$6$#i$w1$q%qiahjkp@y0q8jl$h$$sdfvl*)y5l!d1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
